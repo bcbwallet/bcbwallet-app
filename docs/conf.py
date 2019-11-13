9231f11afe8fc3339a53388b616958658c9118b1
nanli <nanli0709@foxmial.com>
2019-11-13 15:54:09 +0800

    add jsapi.rst

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -23,6 +24,9 @@ author = 'bcbwallet'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0'
+
+
+
 
 
 # -- General configuration ---------------------------------------------------
@@ -48,7 +52,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
